25f2e770277aa789397257cdc9c6be54611be3dc
matthieuzone <matthieu.bruant@gmail.com>
2024-05-01 23:13:50 +0200

    mieux

--- models/mix.py
+++ models/mix.py
@@ -1,13 +1,13 @@
-import dinov2
-import resnet
+import models.dinov2
+import models.resnet
 import torch
 import torch.nn as nn
 
-class mixedModel(nn.Module):
+class MixedModel(nn.Module):
     def __init__(self, p_dino, num_classes, frozen=False, unfreeze_last_layer=True):
-        super(mixedModel, self).__init__()
-        self.dino = dinov2.DinoV2Finetune(num_classes, frozen, unfreeze_last_layer)
-        self.resnet = resnet.ResNet(num_classes, frozen)
+        super(MixedModel, self).__init__()
+        self.dino = models.dinov2.DinoV2Finetune(num_classes, frozen, unfreeze_last_layer)
+        self.resnet = models.resnet.ResNetFinetune(num_classes, frozen)
         self.p_dino = p_dino
 
     def forward(self, x):
