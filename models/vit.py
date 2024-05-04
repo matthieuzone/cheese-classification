1a8872fc78bd66806d767908e2cea046d86025a1
Matthieu <matthieu.bruant@gmail.com>
2024-05-04 15:04:14 +0200

    vit

--- models/vit.py
+++ models/vit.py
@@ -4,28 +4,28 @@ import torchvision
 
 
 class VITFinetune(nn.Module):
-    def __init__(self, num_classes, size = 'B', patch_size = '16', frozen=False, unfreeze_last_layer=True):
+    def __init__(self, num_classes, size = 'B', patch_size = 16, frozen=False, unfreeze_last_layer=True):
         super().__init__()
         if size == 'B':
-            if patch_size == '16':
+            if patch_size == 16:
                 weights = torchvision.models.ViT_B_16_Weights.DEFAULT
                 self.model = torchvision.models.vit_b_16(weights = weights)
-            elif patch_size == '32':
+            elif patch_size == 32:
                 weights = torchvision.models.ViT_B_32_Weights.DEFAULT
                 self.model = torchvision.models.vit_b_32(weights = weights)
             else:
                 raise ValueError('Invalid patch size')
         elif size == 'L':
-            if patch_size == '16':
+            if patch_size == 16:
                 weights = torchvision.models.ViT_L_16_Weights.DEFAULT
                 self.model = torchvision.models.vit_l_16(weights = weights)
-            elif patch_size == '32':
+            elif patch_size == 32:
                 weights = torchvision.models.ViT_L_32_Weights.DEFAULT
                 self.model = torchvision.models.vit_l_32(weights = weights)
             else:
                 raise ValueError('Invalid patch size')
         elif size == 'H':
-            if patch_size == '14':
+            if patch_size == 14:
                 weights = torchvision.models.ViT_H_14_Weights.DEFAULT
                 self.model = torchvision.models.vit_h_14(weights = weights)
             else:
@@ -39,7 +39,7 @@ class VITFinetune(nn.Module):
             if unfreeze_last_layer:
                 for param in self.model.encoder.layers[-1].parameters():
                     param.requires_grad = True
-        self.model.head = nn.Linear(self.model.head.in_features, num_classes)
+        self.model.heads = nn.Linear(self.model.heads[0].in_features, num_classes)
 
     def forward(self, x):
         x = self.model(x)
