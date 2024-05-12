5d6dd13ec4000bc2a40f43b7d9348a01e19f2c7b
topdeliremegagroove <corentin.lucas@psl.eu>
2024-05-12 14:08:54 +0200

    modification des prompts

---
+++ prompts/alea_prompt.py
@@ -0,0 +1,25 @@
+import random
+import os
+
+repertoire = 'prompts/'
+
+def choisir_adjectif(fichier):
+    adjectifs = []
+    with open(os.path.join(repertoire, fichier), 'r') as f:
+        adjectifs += f.readlines()
+    return random.choice(adjectifs).strip()
+
+def choisir_fromage(fichier):
+    fromages = []
+    with open(fichier, 'r') as f:
+        fromages += f.readlines()
+    return random.choice(fromages).strip()
+
+fichiers = [os.path.join(repertoire, 'adjectifs.txt'), os.path.join(repertoire, 'texture.txt'), os.path.join(repertoire, 'vue.txt'), os.path.join(repertoire, 'arriere_plan.txt')]
+
+syntaxe = "Créé une image de " + choisir_fromage('list_of_cheese.txt').lower()+" "+ choisir_adjectif('adjectifs.txt').lower()+ " et " + choisir_adjectif('adjectifs.txt').lower() + " qui est vu " +choisir_adjectif('vue.txt')+ ", placé sur " +choisir_adjectif('arriere_plan.txt')
+
+def create_prompt() :
+    return syntaxe
+
+print(syntaxe)
